a854212b6b38ce50b3304fbb9a2cfdc84cfbb9a0
Markham <quinesquines@gmail.com>
2020-07-01 07:45:58 -0700

    Restart=always; log poweroff, reboot, suspend, hibernate

--- punch-clock-listen.sh
+++ punch-clock-listen.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
 function log () {
-	/usr/local/bin/punch-clock-log.sh $*
+	# /usr/local/bin/punch-clock-log.sh $*
+	echo log  $*
 }
 
 gdbus monitor -y -d org.freedesktop.login1 | while read -r line; do
-	if [[ $line =~ LockedHint ]]; then
-		if [[ $line =~ \<true\> ]]; then
+	if [[ $line =~ org\.freedesktop\.login1\.Session ]]; then
+		if [[ $line =~ \'LockedHint\':\ \<true\> ]]; then
 			log Lock
-		elif [[ $line =~ \<false\> ]]; then
+		elif [[ $line =~ \'LockedHint\':\ \<false\> ]]; then
 			log Unlock
+		fi
+	elif [[ $line =~ org\.freedesktop\.login1\.Manager ]]; then
+		if [[ $line =~ \'PowerOff\':\ \<true\> ]]; then
+			log PowerOff
+		elif [[ $line =~ \'Reboot\':\ \<true\> ]]; then
+			log Reboot
+		elif [[ $line =~ \'Suspend\':\ \<true\> ]]; then
+			log Suspend
+		elif [[ $line =~ \'Hibernate\':\ \<true\> ]]; then
+			log Hibernate
 		fi
 	fi
 done
