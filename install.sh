1fc93c45858055dae86a51ebfacce976b523a0e0
Markham <quinesquines@gmail.com>
2021-11-03 16:11:29 -0700

    Add LTD installation mode

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+if ! systemd 2>/dev/null; then
+	echo 'No systemd available. Just installing limited version'
+	LTD=1
+fi
+
 cp punch-clock-listen.sh /usr/local/bin && \
 cp punch-clock-log.sh /usr/local/bin && \
-cp punch-clock.service /etc/systemd/system && \
+{ (($LTD)) || cp punch-clock.service /etc/systemd/system } && \
 mkdir -p /var/log/punch-clock && \
 chmod o+rwx /var/log/punch-clock && \
-systemctl start punch-clock.service
+{ (($LTD)) || systemctl start punch-clock.service } && \
+echo DONE
